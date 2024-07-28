26fc4387438dc6ffbda526898b0216e32fa79510
DariaKalinina25 <meuw.meuw20@gmail.com>
2024-07-28 17:14:36 +0400

    Configure generators to use Slim by default

--- config/application.rb
+++ config/application.rb
@@ -41,5 +41,8 @@ module SimpleTask
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.template_engine :slim
+    end
   end
 end
